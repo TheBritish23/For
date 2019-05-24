01fb0ea110461e69368d497953be96e0582c0a36
Soro Cat <sorousha326@gmail.com>
2019-05-24 10:41:53 -0700

    add changes in code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+
 $(document).ready(function() {
+  $('form#numbers').submit(function(event){
+    var count_to = parseInt($("#count_to").val());
+    var count_by = parseInt($("#count_by").val());
+    for (var x=0; x<=count_to; x+=count_by ){
+      console.log(x)}
+    event.preventDefault();
+  });
+});
